7950cb0048a8fafd11372d118066d0cbd29b969e
PaisleyMeadow <paisley1000@gmail.com>
2021-02-04 18:34:30 -0700

    implemented med edit, delete med, and changed header bar color + title

--- app/src/main/java/com/paisleydavis/transcribe/Profile.kt
+++ app/src/main/java/com/paisleydavis/transcribe/Profile.kt
@@ -5,11 +5,14 @@ import android.app.NotificationManager
 import android.content.Context
 import android.os.Build
 import android.os.Bundle
+import android.view.View
 import android.widget.Switch
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.NotificationCompat
+import kotlinx.android.synthetic.main.activity_profile.*
 import java.util.*
+
 
 class Profile : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -38,18 +41,75 @@ class Profile : AppCompatActivity() {
         val dateStr = "$dayName, $month $day, $year."
         dateText.text = dateStr
 
+        //hide med edit layout
+        editLayout.visibility = View.GONE
+
+        //hide mood edit layout
+        moodLayout.visibility = View.GONE
+
         @SuppressLint("UseSwitchCompatOrMaterialCode")
         val medReminderSwitch = findViewById<Switch>(R.id.med1Switch)
 
+        //delete medication
+        med1Delete.setOnClickListener {
+            med1Container.visibility = View.GONE
+
+            editLayout.visibility = View.GONE
+        }
+
+        //edit medication
+        med1Add.setOnClickListener {
+            //fill name and dosage
+            val name = med1Name.text.toString()
+            editMedName.setText(name)
+
+            val dose = med1Dose.text.toString()
+            editDosage.setText(dose)
+
+            if (editLayout.visibility == View.VISIBLE) {
+                editLayout.visibility = View.GONE
+            } else {
+                editLayout.visibility = View.VISIBLE
+            }
+        }
+
+        editSubmit.setOnClickListener {
+            editLayout.visibility = View.GONE
+
+            val newName = editMedName.text.toString()
+            val newDose = editDosage.text.toString()
+
+            med1Name.text = newName
+            med1Dose.text = newDose
+        }
+
+        //edit mood
+        addMoodBtn.setOnClickListener {
+            if(moodLayout.visibility == View.GONE){
+                moodLayout.visibility = View.VISIBLE
+            }
+            else{
+                moodLayout.visibility = View.GONE
+            }
+        }
+
         //test notification for reminder switch
-
-
         medReminderSwitch.setOnCheckedChangeListener { _, isChecked ->
             if (isChecked) {
-                //send test notification
+                //change med color
+                
             } else {
                 //do nothing
             }
         }
     }
+
+    //sets saved mood
+    fun setMood(view: View) {
+//        val drawble = resources.getDrawable(R.drawable.ic_training,theme)
+
+//        val chosenMood = resources.getDrawable(R.drawable.tired, theme)
+//                savedMood.setImageDrawable()
+    }
 }
+
